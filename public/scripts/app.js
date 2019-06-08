ceab641a57fdbdf8060d283fee2ae1b39de59e7c
thenullterminator <dazz2803@gmail.com>
2019-06-08 18:47:07 +0530

    dynamic and conditional rendering

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,37 @@
-'use strict';
+"use strict";
+
+var user = {
+    name: "Dhairya Patel",
+    age: 18,
+    location: "Nashik"
+};
+
+function getloc(loc) {
+    if (loc) {
+        return React.createElement(
+            "p",
+            null,
+            "location: ",
+            loc
+        );
+    }
+}
 
 var template = React.createElement(
-  'h1',
-  null,
-  React.createElement(
-    'i',
+    "div",
     null,
-    'This is my first time on react :)'
-  )
+    React.createElement(
+        "h1",
+        null,
+        user.name ? user.name : "Anonymous"
+    ),
+    user.age >= 18 && React.createElement(
+        "p",
+        null,
+        "Age: ",
+        user.age
+    ),
+    getloc(user.location)
 );
 var approot = document.getElementById('app');
 
--- src/app.js
+++ src/app.js
@@ -1,4 +1,24 @@
-var template=<h1><i>This is my first time on react :)</i></h1>;
+
+var user={
+    name:"Dhairya Patel",
+    age:18,
+    location:"Nashik"
+};
+
+function getloc(loc){
+    if(loc)
+    {
+        return <p>location: {loc}</p>
+    }
+}
+
+var template = (
+    <div>
+        <h1>{(user.name)? user.name:"Anonymous"}</h1>
+        {user.age>=18 && <p>Age: {user.age}</p>}
+        {getloc(user.location)}
+    </div>
+);
 var approot=document.getElementById('app');
 
 ReactDOM.render(template,approot);
