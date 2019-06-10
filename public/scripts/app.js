f5f96057e39c2ef05da5215cc62154eb4d4f0ee8
thenullterminator <dazz2803@gmail.com>
2019-06-10 23:42:28 +0530

    some minor changes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,126 +1,36 @@
 'use strict';
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+var vis = false;
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+var toggle = function toggle() {
+    vis = !vis;
+    renderapp();
+};
 
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+var renderapp = function renderapp() {
 
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+    var template = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            'Visibility Toggle'
+        ),
+        React.createElement(
+            'button',
+            { onClick: toggle },
+            'Show Details'
+        ),
+        vis && React.createElement(
+            'p',
+            null,
+            'Hey these are the Details :)'
+        )
+    );
 
-var Counter = function (_React$Component) {
-    _inherits(Counter, _React$Component);
+    var approot = document.getElementById('app');
+    ReactDOM.render(template, approot);
+};
 
-    function Counter(props) {
-        _classCallCheck(this, Counter);
-
-        var _this = _possibleConstructorReturn(this, (Counter.__proto__ || Object.getPrototypeOf(Counter)).call(this, props));
-
-        _this.handleAddOne = _this.handleAddOne.bind(_this);
-        _this.handleSubOne = _this.handleSubOne.bind(_this);
-        _this.handlereset = _this.handlereset.bind(_this);
-        _this.state = {
-            count: 0
-        };
-        return _this;
-    }
-
-    _createClass(Counter, [{
-        key: 'render',
-        value: function render() {
-            return React.createElement(
-                'div',
-                null,
-                React.createElement(
-                    'h1',
-                    null,
-                    'Count: ',
-                    this.state.count
-                ),
-                React.createElement(
-                    'button',
-                    { onClick: this.handleAddOne },
-                    '+1'
-                ),
-                React.createElement(
-                    'button',
-                    { onClick: this.handleSubOne },
-                    '-1'
-                ),
-                React.createElement(
-                    'button',
-                    { onClick: this.handlereset },
-                    'reset'
-                )
-            );
-        }
-    }, {
-        key: 'handleAddOne',
-        value: function handleAddOne() {
-
-            this.setState(function (prevState) {
-                return {
-                    count: prevState.count + 1
-                };
-            });
-        }
-    }, {
-        key: 'handleSubOne',
-        value: function handleSubOne() {
-            this.setState(function (prevState) {
-                return {
-                    count: prevState.count - 1
-                };
-            });
-        }
-    }, {
-        key: 'handlereset',
-        value: function handlereset() {
-            this.setState(function (prevState) {
-                return {
-                    count: 0
-                };
-            });
-        }
-    }]);
-
-    return Counter;
-}(React.Component);
-
-ReactDOM.render(React.createElement(Counter, null), document.getElementById('app'));
-
-// let count=0;
-
-// const inc=()=>{
-//     count++;
-//     renderapp();
-// };
-
-// const dec=()=>{
-//     count--;
-//     renderapp();
-// };
-
-// const reset=()=>{
-//     count=0;
-//     renderapp();
-// };
-
-
-// const renderapp=()=>{
-
-//     const template=(
-//         <div>
-
-//             <h1>Counter: {count} </h1>
-//             <button onClick={inc}>+1</button>
-//             <button onClick={dec}>-1</button>
-//             <button onClick={reset}>reset</button>
-//         </div>
-//     );
-
-//     const approot=document.getElementById('app');
-//     ReactDOM.render(template,approot);
-// };
-
-// renderapp();
+renderapp();
