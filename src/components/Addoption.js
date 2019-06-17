1c3092dc34474f49b5e25f6037e6a9b1b819d694
thenullterminator <dazz2803@gmail.com>
2019-06-17 14:51:30 +0530

    using class properties syntax

--- src/components/Addoption.js
+++ src/components/Addoption.js
@@ -1,26 +1,12 @@
 import React from 'react';
 
 export default class Addoption extends React.Component{
-    constructor(props){
-        super(props);
-        this.addop=this.addop.bind(this);
-        this.state={
-            error:undefined
-        };
-    }
-    render(){
-        return (
-            <div>
-            <p>{this.state.error}</p>
-            <form onSubmit={this.addop}>
-                <input type="text" name="option"/>
-                <button>Add Option</button>
-            </form>
-            </div>
-        );
-    }
 
-    addop(e){
+    state={
+        error:undefined
+    };
+
+    addop=(e)=>{
         console.log("Hello:)");
         e.preventDefault();
         const op=e.target.elements.option.value.trim();
@@ -31,5 +17,17 @@ export default class Addoption extends React.Component{
         }
 
         this.setState(()=>({error}));
+    }
+    
+    render(){
+        return (
+            <div>
+            <p>{this.state.error}</p>
+            <form onSubmit={this.addop}>
+                <input type="text" name="option"/>
+                <button>Add Option</button>
+            </form>
+            </div>
+        );
     }
 }
--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -5,17 +5,45 @@ import Header from './Header';
 import Action from './Action';
  export default class IndecisionApp extends React.Component  {
 
-    constructor(props){
-        super(props);
-        this.handleremoveall=this.handleremoveall.bind(this);
-        this.handlepick=this.handlepick.bind(this);
-        this.addoption=this.addoption.bind(this);
-        this.handleremovesingle=this.handleremovesingle.bind(this);
+    state={
+        ops:[]
+    };
 
-        this.state={
-            ops:[]
+
+    handleremoveall=()=>{
+        this.setState(()=>({ ops:[]}));
+    };
+
+    handleremovesingle=(option)=>{
+        this.setState((prevState)=>{
+            return {
+                ops:prevState.ops.filter((op)=>{
+                    return op!==option;
+                })
+            }
+        });
+    };
+
+    handlepick=()=>{
+        const op=Math.floor(Math.random()*this.state.ops.length);
+        alert(this.state.ops[op]);
+    };
+
+    addoption=(op)=>
+    {
+        if(!op)
+        {
+            return 'Enter a valid value to add option.'
         }
-    }
+        else if(this.state.ops.indexOf(op)!=-1)
+        {
+            return 'The Option already exist.'
+        }
+
+        this.setState((prevState)=>({ ops:prevState.ops.concat([op]) }));
+    };
+
+
     componentDidUpdate(prevProps,prevState)  {
         if(prevState.ops.length!==this.state.ops.length)  {
             localStorage.setItem('DataBase',JSON.stringify(this.state.ops));
@@ -35,38 +63,7 @@ import Action from './Action';
         catch(e){}
     }
 
-    handleremoveall(){
-        this.setState(()=>({ ops:[]}));
-    }
-
-    handleremovesingle(option){
-        this.setState((prevState)=>{
-            return {
-                ops:prevState.ops.filter((op)=>{
-                    return op!==option;
-                })
-            }
-        });
-    }
-
-    handlepick(){
-        const op=Math.floor(Math.random()*this.state.ops.length);
-        alert(this.state.ops[op]);
-    }
-
-    addoption(op)
-    {
-        if(!op)
-        {
-            return 'Enter a valid value to add option.'
-        }
-        else if(this.state.ops.indexOf(op)!=-1)
-        {
-            return 'The Option already exist.'
-        }
-
-        this.setState((prevState)=>({ ops:prevState.ops.concat([op]) }));
-    }
+  
 
    render(){
         const title="Indecision App";
